60353ed874e243cf31953c694f36b573dd2fdd4f
ludmyllavaladares <ludmylla.valadares@a.ficr.edu.br>
2021-03-25 10:55:46 -0300

    criação do indexjs

---
+++ index.js
@@ -0,0 +1,53 @@
+const nomePetshop = "PETSHOP AVANADE";
+
+let pets = [
+    {
+        nome: 'Sansão',
+        tipo: 'cachorro',
+        idade: 8,
+        reca: 'schnauzer',
+        tutor: 'Lud',
+        contato: '(81) 99999-0000',
+        vacinado: true,
+        serviços: ['banho', 'tosa', 'vacina', 'hotelzinho']
+    },
+    {
+        nome: 'Biscoito',
+        tipo: 'gato',
+        idade: 2,
+        reca: 'SDR',
+        tutor: 'Bruno',
+        contato: '(81) 92222-8888',
+        vacinado: false,
+        serviços: ['banho']
+    },
+    {
+        nome: 'Dalila',
+        tipo: 'cachorro',
+        idade: 5,
+        reca: 'pastor-alemão',
+        tutor: 'Pedro',
+        contato: '(81) 91111-5555',
+        vacinado: true,
+        serviços: ['banho','vacina']
+    },
+    {
+        nome: 'Frida',
+        tipo: 'cachorro',
+        idade: 13,
+        raca: 'schnauzer',
+        tutor: 'João Henrique',
+        contato: '(81) 99999-4565',
+        vacinado: true,
+        serviços: ['banho', 'tosa', 'vacina', 'medicação']
+    },
+];
+
+const listarPets = () => {
+    for(let i = 0; i < pets.length; i++){
+        console.log(pets[i].nome);
+    }
+}
+
+listarPets();
+// console.log(pets);
